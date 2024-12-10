3c93efb575645b20bd42324dfffa95bc55504b0e
Piotr Bobiński WSB <62461206+pbobinski-WSB@users.noreply.github.com>
2024-12-10 17:37:22 +0100

    todovaadin added

---
+++ todoVaadin/src/main/java/com/example/application/views/main/Todo.java
@@ -0,0 +1,36 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("ToDo")
+@Route(value = "todo")
+public class Todo extends VerticalLayout {
+
+    public Todo() {
+        VerticalLayout todosList = new VerticalLayout();
+        TextField taskField = new TextField();
+        Button addButton = new Button("Add");
+        addButton.addClickListener(click -> {
+            Checkbox checkbox = new Checkbox(taskField.getValue());
+            todosList.add(checkbox);
+        });
+        addButton.addClickShortcut(Key.ENTER);
+
+        add(
+                new H1("Vaadin Todo"),
+                todosList,
+                new HorizontalLayout(
+                        taskField,
+                        addButton
+                )
+        );
+    }
+}
